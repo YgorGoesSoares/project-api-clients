2be06177f8568a88a9a9babfd0e3a97ab2b3621d
YgorGoesSoares <108436676+YgorGoesSoares@users.noreply.github.com>
2024-04-23 03:43:50 -0300

    Backend project created

---
+++ Clients-crud/src/test/java/com/project/crud/clients/Clientscrud/ClientsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crud.clients.Clientscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
